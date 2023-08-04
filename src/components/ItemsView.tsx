0700b6e200b61d6a10411c67ec172d32c28f62aa
Raxabi <beltrandiaztorres@gmail.com>
2023-08-04 12:07:43 +0200

    [ADD] add flex style to items renderization and little description of function at jsdoc

--- src/components/ItemsView.tsx
+++ src/components/ItemsView.tsx
@@ -1,5 +1,5 @@
+//import { invoke } from "@tauri-apps/api"
 import { type FileEntry } from "@tauri-apps/api/fs"
-
 import Item, { type ItemType } from "./mini/Item"
 
 interface ItemsViewProps {
@@ -7,13 +7,13 @@ interface ItemsViewProps {
 }
 
 /**
- *
+ * Contains all the items from the readed dir
  * @param {ItemsViewProps} items Array containing all the files and directories (items) in the actual directory
  * @returns
  */
 function ItemsView({items}: ItemsViewProps) {
   return (
-    <div>
+    <div className="flex flex-col gap-y-5 px-10 pt-5 pb-10">
       {items.map(item => {
         const itemType: ItemType = item.children ? "Folder" : "File";
         return <Item key={item.name} itemType={itemType} itemName={item.name}/>
