abf952294c9e369be01ec6f8c6bb4ca3df3cabcf
Raxabi <beltrandiaztorres@gmail.com>
2023-08-04 12:11:08 +0200

    [ADD] added image for directories and files and flex style

--- src/components/mini/Item.tsx
+++ src/components/mini/Item.tsx
@@ -1,3 +1,10 @@
+import { readBinaryFile, BaseDirectory } from "@tauri-apps/api/fs"
+
+const { URL } = globalThis
+
+const fileIcon = URL.createObjectURL(new Blob([await readBinaryFile("file_explorer_icons/file.png", { dir: BaseDirectory.AppConfig })]))
+const dirIcon = URL.createObjectURL(new Blob([await readBinaryFile("file_explorer_icons/folder.png", { dir: BaseDirectory.AppConfig })]))
+
 export type ItemType = "File" | "Folder"
 
 interface ItemProps {
@@ -7,9 +14,9 @@ interface ItemProps {
 
 function Item(props: ItemProps) {
   return (
-    <div>
-      <img />
-      {props.itemName}
+    <div className="flex items-center">
+      <img src={props.itemType === "File" ? fileIcon : dirIcon}/>
+      <p>{props.itemName}</p>
     </div>
   )
 }
